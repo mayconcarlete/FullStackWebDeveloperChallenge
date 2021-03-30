d86c716e482bd392b7164ed4270016cbd9ec4d53
Maycon Carlete <maycon.carlete@hotmail.com>
2021-03-30 20:02:17 -0300

    feat: ensure ValidatorComposite return first error when validate

---
+++ src/presentation/validators/validator-composite.ts
@@ -0,0 +1,13 @@
+import { IValidate } from "@presentation/protocols/validate";
+
+export class ValidatorComposite implements IValidate{
+    constructor(
+        private readonly validation:IValidate[]
+    ){}
+    validate(input: any): Error | undefined {
+        for(const validator of this.validation){
+            const error = validator.validate(input)
+            if(error) return error
+        }
+    }
+}
