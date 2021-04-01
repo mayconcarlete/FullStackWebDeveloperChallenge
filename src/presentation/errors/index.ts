56a258a178972cfa13824a26b208a51ad292c60d
Maycon Carlete <maycon.carlete@hotmail.com>
2021-04-01 20:54:01 -0300

    feat: ensure TypeVerification return undefined when validation succeeds

--- src/presentation/errors/index.ts
+++ src/presentation/errors/index.ts
@@ -1,1 +1,2 @@
 export {MissingParamError} from './missing-param-error'
+export {TypeVerificationError} from './type-verification-error'
---
+++ src/presentation/validators/type-verification.ts
@@ -0,0 +1,12 @@
+import { TypeVerificationError } from "@presentation/errors/type-verification-error";
+import { IValidate } from "@presentation/protocols/validate";
+
+export class TypeVerification implements IValidate{
+    constructor(
+        private readonly fieldName: string,
+        private readonly type: string
+        ){}
+    validate(input: any): Error | undefined {
+        if(typeof(input[this.fieldName]) !== this.type) return new TypeVerificationError(this.fieldName)
+    }
+}
