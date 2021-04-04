984f1cf8122cb1fa00cd710ba01e2576d9476837
Maycon Carlete <maycon.carlete@gmail.com>
2021-04-04 14:40:43 -0300

    test: ensure validators returns undefined when validation succeeds

--- tests/presentation/controllers/get-three-similar-words.spec.ts
+++ tests/presentation/controllers/get-three-similar-words.spec.ts
@@ -47,13 +47,11 @@ describe('Get Three Words class', () => {
         const response = await sut.handle(request)
         expect(response.body).toEqual(new TypeVerificationError('word'))
         expect(response.statusCode).toBe(400)
-    })/*
-    test('Should return an array with three similar results', async() => {
-        const {sut} = makeSut()
-        const request:THttpRequest = {
-            params:{word:'car'}
-        }
-        const response = await sut.handle(request)
-
-    })*/
+    })
+    test('Should return undefined if validation succeeds', async () => {
+        const {sut, validators} = makeSut()
+        const request:THttpRequest = {params:{word:'any_word'}}
+        await sut.handle(request)
+        expect(validators.error).toBeFalsy()
+    })
 })
