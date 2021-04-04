0e852dabf7dcc539113c6786ca72cc0a5702f4d7
Maycon Carlete <maycon.carlete@gmail.com>
2021-04-04 10:20:17 -0300

    test: ensure GetThreeWords controller return bad request when no word is provided

--- tests/presentation/controllers/get-three-similar-words.spec.ts
+++ tests/presentation/controllers/get-three-similar-words.spec.ts
@@ -1,23 +1,29 @@
 import { GetThreeWords } from '@presentation/controllers'
+import { MissingParamError } from '@presentation/errors'
 import {IController} from '@presentation/protocols'
 import { THttpRequest } from '@presentation/types'
+import { RequiredField, ValidatorComposite } from '@presentation/validators'
+import { TypeVerification } from '@presentation/validators/type-verification'
 
 type SutTypes = {
     sut: IController 
 }
 
 const makeSut = ():SutTypes => {
-    const sut = new GetThreeWords()
+    const arrayOfValidations = []
+    arrayOfValidations.push(new RequiredField('word'))
+    arrayOfValidations.push(new TypeVerification('word', 'string'))
+    const validatorCoposite = new ValidatorComposite(arrayOfValidations)
+    const sut = new GetThreeWords(validatorCoposite)
     return {sut}
 }
 
 describe('Get Three Words class', () => {
     test('Should return 400 if no word is provided', async () => {
         const {sut} = makeSut()
-        const request:THttpRequest = {
-            body:{
-                "invalid_field": "any_value"
-            }
-        }
+        const request:THttpRequest = {}
+        const response = await sut.handle(request)
+        expect(response.body).toEqual(new MissingParamError('word'))
+        expect(response.statusCode).toBe(400)
     })
 })
