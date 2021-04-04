1e95e97b0994d0dd489a2391885085c3f96a0bf0
Maycon Carlete <maycon.carlete@gmail.com>
2021-04-04 15:28:28 -0300

    refactor: remove database from index file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import 'module-alias/register'
-import {Database} from './infra/database/index'
 import {createFakeConnection} from './infra/database/fake-database-config'
 import server from './main/app'
 
 createFakeConnection().then( instanceConnection => {
-    const db = Database.getInstance( instanceConnection )
     server.listen(3000, () => {
-        console.log('Ligooou')
+        console.log('Uiihaaaa')
     })
 })
