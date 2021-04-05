37a0675f05c270a53b30ec84e5fa23e91d664cf6
Maycon Carlete <maycon.carlete@hotmail.com>
2021-04-04 22:48:14 -0300

    test: return right values to pass tests - husky and lint-staged tests

--- tests/presentation/errors/type-verification.spec.ts
+++ tests/presentation/errors/type-verification.spec.ts
@@ -21,7 +21,7 @@ describe('Type Verification class', () => {
             [fieldName]:"any_value"
         }
         sut.validate(body)
-        expect(validateSpy).toHaveBeenCalledWith({[fieldName]:"any_values"})
+        expect(validateSpy).toHaveBeenCalledWith({[fieldName]:"any_value"})
     })
 
     test('Should return an error if type of field are different of passed', () => {
