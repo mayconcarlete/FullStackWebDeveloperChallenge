6684eef5cd88436640bd5b44efcc14f2f783e5b1
Maycon Carlete <maycon.carlete@hotmail.com>
2021-04-07 21:50:37 -0300

    chore: remove infra/corpus database from collect coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/index.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/infra/**'
+  ],
   moduleNameMapper:{
     "@presentation/(.*)": "<rootDir>/src/presentation/$1",
     "@domain/(.*)":"<rootDir>/src/domain/$1",
