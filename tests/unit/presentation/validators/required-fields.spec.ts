742ede39552542f0a8c33d4ce8c1b436c8e01472
Maycon Carlete <maycon.carlete@hotmail.com>
2021-04-07 21:20:11 -0300

    test: ensure RequiredFields return undefined when validation succeeds

---
+++ tests/unit/presentation/validators/required-fields.spec.ts
@@ -0,0 +1,31 @@
+import { MissingParamError } from "@presentation/errors"
+import { IValidate } from "@presentation/protocols/validate"
+import { RequiredField } from "@presentation/validators"
+
+const fieldName = 'valid_field'
+
+type SutTypes = {
+    sut:IValidate
+}
+
+const makeSut = ():SutTypes => {
+    const sut = new RequiredField(fieldName)
+    return {sut}
+}
+
+describe('Required Field class', () =>{
+    test('Should return MissingParamErro when word is missing', () => {
+        const {sut} = makeSut()
+        const input = {}
+        const requiredFieldsResult = sut.validate(input)
+        expect(requiredFieldsResult).toEqual(new MissingParamError(fieldName))
+    })
+    test('Should return undefined when RequiredFields succeeds', () => {
+        const {sut} = makeSut()
+        const input = {
+            [fieldName]:'any_value'
+        }
+        const requiredFieldsResult = sut.validate(input)
+        expect(requiredFieldsResult).toBeFalsy()
+    })
+})
