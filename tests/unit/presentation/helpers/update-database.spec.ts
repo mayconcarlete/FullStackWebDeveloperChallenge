4e5c4b1bcf94eb9b7f0c1248eee6408191989805
Maycon Carlete <maycon.carlete@hotmail.com>
2021-04-10 16:45:50 -0300

    test: ensure InsertDatabase return false when word alread exists in db

--- tests/unit/presentation/helpers/update-database.spec.ts
+++ tests/unit/presentation/helpers/update-database.spec.ts
@@ -1,16 +1,27 @@
 import { InsertDatabase } from "@presentation/helpers/insert-database"
 import { IUpdateDabase } from "@presentation/protocols"
+import { MockDatabase } from "../mocks/mock-database"
 
 type SutTypes = {
     sut: IUpdateDabase
+    databaseSpy: MockDatabase
 }
 
 const makeSut = ():SutTypes => {
-    const sut = new InsertDatabase()
-    return {sut}
+    const databaseSpy = new MockDatabase()
+    const sut = new InsertDatabase(databaseSpy)
+    return {sut, databaseSpy}
 }
 
 describe('Insert Database class', () => {
+    test('Should return false when word already exists in db', async () => {
+        const {sut, databaseSpy} = makeSut()
+        const word = 'valid_word'
+        databaseSpy.resultOfInsert = false
+        const resultOfInsert = await sut.insert(word)
+        expect(resultOfInsert).toBeFalsy()
+    })
+
     test('Should return an updated word when insert in database', async () => {
         const {sut} = makeSut()
         const wordToUpdate = 'valid_word'
