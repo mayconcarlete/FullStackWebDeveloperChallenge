f380e6f2263a8aaf1ada10f95dfba2a2d36a994a
Maycon Carlete <maycon.carlete@gmail.com>
2021-04-11 12:01:15 -0300

    feat: ensure Database returns true when word is add

--- src/infra/database/index.ts
+++ src/infra/database/index.ts
@@ -1,7 +1,8 @@
 import { IDatabase } from "@presentation/protocols/database"
+import { IInsertDatabase } from "@presentation/protocols/insert-database"
 
 
-export class Database implements IDatabase {
+export class Database implements IDatabase, IInsertDatabase {
     private static instance: Database
     
     private constructor(
@@ -16,5 +17,11 @@ export class Database implements IDatabase {
     }    
     async getDatabase():Promise<string[]> {
         return new Promise( resolve => resolve(this.database))
+    }
+
+    async create(word: string): Promise<boolean> {
+        return new Promise(resolve => {
+            resolve(true)
+        })
     }
 }
