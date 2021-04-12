907b6fe6d65b2d2902e21c19501c8bd2298900ff
Maycon Carlete <maycon.carlete@hotmail.com>
2021-04-11 22:44:14 -0300

    docs: prepare api to swagger documentation

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,8 +1,12 @@
 import express from 'express'
+import setupSwagger from './routes/swagger-route'
 import getWords from './routes/get-three-words'
 import updateWord from './routes/update-word'
 
 const app = express()
+
+setupSwagger(app)
+
 app.use(express.json())
 
 getWords(app)
