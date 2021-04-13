db5240ee05422b431bb35b79ee914ffcabf175f1
Maycon Carlete <maycon.carlete@hotmail.com>
2021-04-12 22:53:10 -0300

    feat: create DeleteWordController factory

--- src/main/app.ts
+++ src/main/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import setupSwagger from './routes/swagger-route'
 import getWords from './routes/get-three-words'
 import updateWord from './routes/update-word'
+import deleteWord from './routes/delete-word'
 
 const app = express()
 
@@ -11,5 +12,6 @@ app.use(express.json())
 
 getWords(app)
 updateWord(app)
+deleteWord(app)
 
 export default app
---
+++ src/main/factory/delete-word/make-delete-word-controller.ts
@@ -0,0 +1,24 @@
+import { Database } from "@infra/database";
+import { createFakeConnection } from "@infra/database/fake-database-config";
+import { DeleteWordController } from "@presentation/controllers/delete-word-database";
+import { DeleteDatabase } from "@presentation/helpers/delete-database";
+import { ParseInt, RequiredField, TypeVerification, ValidatorComposite } from "@presentation/validators";
+import {Express} from 'express'
+export const makeDeleteWordController = async ():Promise<DeleteWordController> => {
+    const fieldName = 'word'
+
+    const arrayOfValidators = []
+    arrayOfValidators.push(new RequiredField(fieldName))
+    arrayOfValidators.push(new TypeVerification(fieldName, 'string'))
+    arrayOfValidators.push(new ParseInt(fieldName))
+
+    const validators = new ValidatorComposite(arrayOfValidators)
+
+    const createDatabaseFromTxt = await createFakeConnection()
+    const database = Database.getInstance(createDatabaseFromTxt)
+    const deleteDatabase = new DeleteDatabase(database)
+    
+    const deleteWordController = new DeleteWordController(validators, deleteDatabase)
+
+    return deleteWordController
+}
