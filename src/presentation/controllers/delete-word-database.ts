8ef09e0bfbce2ea1fcfb0882717d22c80c39eb0a
Maycon Carlete <maycon.carlete@hotmail.com>
2021-04-12 21:59:40 -0300

    feat: ensure DeleteWordController return bad request when validations fails

--- src/presentation/controllers/delete-word-database.ts
+++ src/presentation/controllers/delete-word-database.ts
@@ -1,15 +1,20 @@
 import { NotExistsInDatabase } from "@presentation/errors";
 import { badRequest, ok, serverError } from "@presentation/helpers";
-import { IController } from "@presentation/protocols";
+import { IController, IValidate } from "@presentation/protocols";
 import { IDeleteDatabase } from "@presentation/protocols/delete-database";
 import { THttpRequest, THttpResponse } from "@presentation/types";
 
 export class DeleteWordController implements IController{
     constructor(
+        private readonly validators: IValidate,
         private readonly deleteDatabase: IDeleteDatabase
     ){}
     async handle(request: THttpRequest): Promise<THttpResponse> {
         try{
+            const error = this.validators.validate(request.params)
+            if(error){
+                return badRequest(error)
+            }
             const wordToDelete = request.params.word
             const resultOfDelete = await this.deleteDatabase.delete(wordToDelete)
             if(!resultOfDelete){
