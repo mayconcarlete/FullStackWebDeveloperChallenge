34be6199bacef5f4e95ceb14b3cf57d71ef3c303
Maycon Carlete <maycon.carlete@hotmail.com>
2021-04-13 21:01:40 -0300

    feat: swagger cache control and initial config

--- src/main/swagger/index.ts
+++ src/main/swagger/index.ts
@@ -1,3 +1,8 @@
 export default {
-    
+    openapi: '3.0.0',
+    info:{
+        title: 'Mistplay FullStack Challenge',
+        description: 'Api maded to make search, insert and post in a given database',
+        version: '1.0.0'
+    }
 }
