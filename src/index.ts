e09a9e5dd6091ecb6e2ef7e8c00d88b915a63e00
Maycon <maycon.carlete@gmail.com>
2021-07-25 16:53:10 -0300

    docker: create a image to unit-tests

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import 'module-alias/register'
 import server from './main/app'
 
-const PORT = process.env.PORT ?? 3000
+const PORT = process.env.SERVER_PORT ?? 3000
 
 server.listen(PORT, () => {
   console.log(`We are online on port: ${PORT}!!!`)
