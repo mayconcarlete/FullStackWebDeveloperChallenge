1580a621a3e5b473eb21cf59be09a9cbb2c5f1ac
Maycon Carlete <maycon.carlete@gmail.com>
2021-11-08 10:10:15 -0300

    chore: set clearMocks flag to true

--- jest.config.js
+++ jest.config.js
@@ -7,10 +7,11 @@ module.exports = {
     '!<rootDir>/src/main/**',
     '!<rootDir>/src/infra/**'
   ],
-  moduleNameMapper:{
-    "@presentation/(.*)": "<rootDir>/src/presentation/$1",
-    "@domain/(.*)":"<rootDir>/src/domain/$1",
-    "@main/(.*)":"<rootDir>/src/main/$1",
-    "@infra/(.*)": "<rootDir>/src/infra/$1"
-  }
-};
+  moduleNameMapper: {
+    '@presentation/(.*)': '<rootDir>/src/presentation/$1',
+    '@domain/(.*)': '<rootDir>/src/domain/$1',
+    '@main/(.*)': '<rootDir>/src/main/$1',
+    '@infra/(.*)': '<rootDir>/src/infra/$1'
+  },
+  clearMocks: true
+}
